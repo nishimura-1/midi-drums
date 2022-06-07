fb45eddebaac4c167afbed1d178a1243655a1b0c
nishimura <megan.ouchida@gmail.com>
2022-06-07 16:18:50 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const hitTriggers = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
+
+const drums = document.querySelectorAll('.hit')
+
+drums.forEach(drum => {
+    drum.addEventListener('click', () => playDrum(drum))
+})
+
+document.addEventListener('keydown', e => {
+    const drum = e.key
+    const triggerIndex = hitTriggers.indexOf(drum)
+
+    if (triggerIndex > -1) playDrum(drums[triggerIndex])
+})
+
+function playDrum(drum) {
+    const drumAudio = document.getElementById(drum.dataset.type)
+    drumAudio.currentTime = 0
+    drumAudio.play()
+    drum.classList.add('active')
+    drumAudio.addEventListener('ended', () => {
+        drum.classList.remove('active')
+    })
+}
